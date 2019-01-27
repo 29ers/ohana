fdf0f5826f9ad45c140a43c833210e18c9723d6f
Russell Reiter <russell.reiter@gmail.com>
2019-01-27 14:41:41 -0600

    Fuck it

--- src/scenes/levelOne.js
+++ src/scenes/levelOne.js
@@ -1,13 +1,15 @@
 import Phaser from 'phaser';
 
 var player;
-var cursors;
-// var platforms;
 var resourcesGathered = 0;
+var resourcesGatheredText;
 var resourcesMax = 100;
 var resourcesDropped = 0;
 var playerHealth = 100;
-
+var map;
+var groundLayer;
+var stars;
+var cursors;
 
 export class LevelOne extends Phaser.Scene {
     constructor () {
@@ -23,29 +25,27 @@ export class LevelOne extends Phaser.Scene {
         this.load.spritesheet('tiles', 'assets/tiles.png', {frameWidth: 70, frameHeight: 70});
         // this.load.image('platforms', 'assets/ground.png');
         this.load.image('background', 'assets/Forest.png');
+        this.load.image('star', 'assets/invader.png');
         this.load.spritesheet('player', 'assets/player.png', { frameWidth: 128, frameHeight: 120});
     }
 
     create () {
 
-        this.background = this.add.tileSprite(window.innerWidth/2.35, window.innerHeight/2.06,1200,800, 'background')
-        var map = this.make.tilemap({ key: 'map' })
+        this.background = this.add.tileSprite(window.innerWidth/2.23, window.innerHeight/2.15,1200,800, 'background')
+        map = this.make.tilemap({ key: 'map' })
         var groundTiles = map.addTilesetImage('tiles')
-        var groundLayer = map.createDynamicLayer('World', groundTiles, 1, 200)
-        // groundLayer.setCollisionByExcluision([-1])
+        groundLayer = map.createDynamicLayer('World', groundTiles, 1, 200)
+        groundLayer.setCollisionByExclusion([-1]);
 
         this.physics.world.bounds.width = groundLayer.width;
         this.physics.world.bounds.height = groundLayer.height;
 
-        // platforms = this.physics.add.staticGroup();
+        // groundTiles = this.physics.add.staticGroup();
         // platforms.create(500,800, 'platforms').setScale(4).refreshBody();
-
         player = this.physics.add.sprite(100, 200, 'player')
         player.setBounce(0.2);
         player.setCollideWorldBounds(true);
-        player.body.setSize(player.width, player.height-8);
-
-        // this.physics.add.collider(player, groundLayer)
+        // player.body.setSize(player.width, player.height-8);
 
         // this.physics.add.overlap(player, resource, collectResources, null, this);
         // this.physics.add.overlap(player, bomb, collectResources, null, this);
@@ -75,8 +75,40 @@ export class LevelOne extends Phaser.Scene {
             repeat: -1
 
         })
-
         cursors = this.input.keyboard.createCursorKeys();
+
+        stars = this.physics.add.group({
+            key: 'star',
+            repeat: 15,
+            setXY: {x: 12, y: 0, stepX: 140}
+        })
+
+        stars.children.iterate(function (child) {
+
+            child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8))
+        });
+
+        this.physics.add.collider(player, groundLayer);
+        this.physics.add.collider(stars, groundLayer);
+        this.physics.add.collider(stars, player);
+
+
+        resourcesGatheredText = this.add.text(16, 16, "Resources Gathered: 0", {fontSize: '32px', fill: '#fff'})
+        // set bounds so the camera won't go outside the game world
+        this.cameras.main.setBounds(75, 100, map.widthInPixels, map.heightInPixels);
+        // make the camera follow the player
+        this.cameras.main.startFollow(player);
+
+        // set background color, so the sky is not black
+        this.cameras.main.setBackgroundColor('#ccccff');
+
+        this.input.keyboard.on('keydown_' + 'SPACE', () => {this.scene.start('bossOne')});
+
+        // this.physics.add.overlap(player, star, (star) =>
+        //     {star.disableBody(true, true);
+        //     resourcesGathered += 1;
+        //     resourcesGatheredText.updateText("Resources Gathered: " + resourcesGathered);}, null, this)
+
     }
 
     update () {
@@ -96,12 +128,13 @@ export class LevelOne extends Phaser.Scene {
             player.anims.play('turn', true)
         }
 
-        if (cursors.up.isDown && player.body.touching.down) {
+        if (cursors.up.isDown && player.body.onFloor()) {
 
             player.setVelocityY(-370)
         }
 
         this.background.tilePositionX += direction*5
+
 
         if (resourcesGathered === resourcesMax || resourcesDropped === 100) {
 
@@ -114,11 +147,11 @@ export class LevelOne extends Phaser.Scene {
         }
     }
 
-    collectResources (resource) {
+    collectStar (star) {
 
-        resource.disableBody(true, true);
+        star.disableBody(true, true);
         resourcesGathered += 1;
-        resourcesGatheredText.setText('Resources Gathered: ' + score);
+        resourcesGatheredText.updateText("Resources Gathered: " + resourcesGathered);
 
     }
 
--- src/scenes/menu.js
+++ src/scenes/menu.js
@@ -11,7 +11,7 @@ export class Menu extends Phaser.Scene {
 
         let nameLabel = this.add.text(window.innerWidth/2.8, 200, 'OHANA', {fontSize: '50px', fill: '#fff'});
         let startLabel = this.add.text(400, 400, 'Press the "W" key to start', {fontSize: '25px', fill: '#fff'});
-        this.input.keyboard.on('keydown_' + 'W', () => {this.scene.start('levelFour')});
+        this.input.keyboard.on('keydown_' + 'W', () => {this.scene.start('levelOne')});
 
     }
 }
